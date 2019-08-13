e7e7a535cd744130348fa2b608c9b0ed8902186a
Hans Elias B. Josephsen <me@hansihe.com>
2019-08-13 12:00:11 +0200

    more patch debug

--- otp_build/patch_tests.sh
+++ otp_build/patch_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-patch --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch
+cat bs_match_SUITE.erl.patch
+cat ../otp/lib/compiler/test/bs_match_SUITE.erl
+
+patch --normal --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch
 
