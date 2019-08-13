f6aec88657172a1db74f9260bdbc0537dce6c852
Hans Elias B. Josephsen <me@hansihe.com>
2019-08-13 12:41:27 +0200

    more diagnostics

--- otp_build/patch_tests.sh
+++ otp_build/patch_tests.sh
@@ -4,5 +4,6 @@ set -e
 cat bs_match_SUITE.erl.patch
 cat ../otp/lib/compiler/test/bs_match_SUITE.erl
 
-patch -l --normal --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch
+patch -l --normal --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch || true
+cat bs_match_SUITE_patched.erl.rej
 
