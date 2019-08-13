1eddf9f09f887ab6efc2eca488f2e09d1386bc70
Hans Elias B. Josephsen <me@hansihe.com>
2019-08-13 12:06:05 +0200

    ignore line endings in patch

--- otp_build/patch_tests.sh
+++ otp_build/patch_tests.sh
@@ -4,5 +4,5 @@ set -e
 cat bs_match_SUITE.erl.patch
 cat ../otp/lib/compiler/test/bs_match_SUITE.erl
 
-patch --normal --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch
+patch -l --normal --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch
 
