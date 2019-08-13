061fc1b870273aa07707cf611b60b5decf9cdfe9
Hans Elias B. Josephsen <me@hansihe.com>
2019-08-13 13:10:59 +0200

    change test patch script to copy files first

--- otp_build/patch_tests.sh
+++ otp_build/patch_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-cat bs_match_SUITE.erl.patch
-cat ../otp/lib/compiler/test/bs_match_SUITE.erl
-
-patch -l --normal --verbose -o bs_match_SUITE_patched.erl ../otp/lib/compiler/test/bs_match_SUITE.erl bs_match_SUITE.erl.patch || true
-cat bs_match_SUITE_patched.erl.rej
+cp ../otp/lib/compiler/test/bs_match_SUITE.erl .
+patch -l --normal --verbose bs_match_SUITE.erl bs_match_SUITE.erl.patch
 
