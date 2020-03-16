c3ca08a8613c119fcb567a6aa98df467eb812292
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-03-16 17:14:54 -0400

    Print better information when validation pass panics

--- libeir_passes/src/validate.rs
+++ libeir_passes/src/validate.rs
@@ -21,6 +21,6 @@ impl FunctionPass for ValidatePass {
     fn run_function_pass(&mut self, b: &mut FunctionBuilder) {
         self.err_buf.clear();
         b.fun().validate(&mut self.err_buf);
-        assert!(self.err_buf.len() == 0);
+        assert_eq!(self.err_buf.len(), 0, "{:?}", &self.err_buf);
     }
 }
