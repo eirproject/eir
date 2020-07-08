4096af72bc358df17f7796770dc861067a0c80e5
Hans Elias B. Josephsen <me@hansihe.com>
2020-07-08 16:20:42 +0200

    Fix unimplemented `op_branch_target` for `DynOp`

--- libeir_ir/src/algo/op_branches.rs
+++ libeir_ir/src/algo/op_branches.rs
@@ -48,7 +48,9 @@ impl Function {
         let res = match self.block_kind(block)? {
             OpKind::Call(CallKind::ControlFlow) => 1,
             OpKind::Call(CallKind::Function) => 2,
+
             OpKind::Unreachable => 0,
+
             OpKind::IfBool => {
                 let reads = self.block_reads(block);
                 match reads.len() {
@@ -57,22 +59,18 @@ impl Function {
                     _ => unreachable!(),
                 }
             }
-            OpKind::Match { branches } => branches.len(),
+
             OpKind::TraceCaptureRaw => 1,
             OpKind::TraceConstruct => 1,
             OpKind::UnpackValueList(_) => 1,
             OpKind::MapPut { .. } => 2,
+
+            OpKind::Match { branches } => branches.len(),
+
             OpKind::Dyn(dyn_op) => {
                 let op_branches = self.dialect().get_op_branches(&**dyn_op).unwrap();
                 op_branches.branches_len()
-            } //OpKind::Intrinsic(name) => {
-              //    match name.as_str().get() {
-              //        "receive_start" => 1,
-              //        "receive_wait" => 2,
-              //        "receive_done" => 1,
-              //        _ => unimplemented!(),
-              //    }
-              //}
+            }
         };
 
         Some(res)
@@ -100,18 +98,10 @@ impl Function {
 
             (OpKind::Match { .. }, _, n) => self.value_list_get_n(reads[0], n).unwrap(),
 
-            (OpKind::Dyn(_dyn), _, _) => unimplemented!(),
-            //(OpKind::Intrinsic(name), _, n) => {
-            //    match name.as_str().get() {
-            //        // receive_start only has a single branch target
-            //        "receive_start" if n == 0 => reads[0],
-            //        // receive_wait only has two branch targets
-            //        "receive_wait" if n < 2 => reads[n],
-            //        // receive_done only has a single branch target
-            //        "receive_done" if n == 0 => reads[0],
-            //        _ => unimplemented!(),
-            //    }
-            //}
+            (OpKind::Dyn(dyn_op), _, _) => {
+                let op_branches = self.dialect().get_op_branches(&**dyn_op).unwrap();
+                op_branches.branch_num(self, block, n)
+            }
             _ => panic!(),
         }
     }
